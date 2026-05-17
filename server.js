c0ce88aafaa486ba1f0cb43e35a11d147aa982aa
davido <tngh1562@gmail.com>
2026-05-17 11:39:02 +0900

    Restore top navigation shell

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'inhouse.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
